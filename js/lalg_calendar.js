4147b49bff60c0a669cc30c3a6f303ff29a1ed9e
lalgwebdev <47321570+lalgwebdev@users.noreply.github.com>
2024-04-04 10:03:29 +0100

    New calendar functionality

--- js/lalg_calendar.js
+++ js/lalg_calendar.js
@@ -1,13 +1,141 @@
 (function( $, Drupal ) {
+	
+  Drupal.behaviors.calendarJump = {
+    attach: function (context, settings) {
+	  $("button.button_today").click(function(){
+	    let todaydate = $('div.calendar-calendar td.today:first');
+	    $("body, html").animate({	scrollTop: $( todaydate ).offset().top }, 600);	 
+	  });	
+    }
+  }
+  
 
- 
+  Drupal.behaviors.calendarClicks = {
+    attach: function (context, settings) {
+		
+	  function changeofmonth() {
+		// When displaying the calendar, add a class which represents the group type  
+		$(".hide").each(function() {
+		  $(this).parents(".calendar").addClass($(this).text().trim().toLowerCase().split(" ").join(""));
+		});  
+	  
+		if ($('#block-lalgcalendarlegend li.group_type_show').length > 0) {
+			$('#block-lalgcalendarlegend li.group_type_show').click();	
+		}
+	  }
+	  
+	  function getMonthName(fullDate) {
+		let monthNumber = fullDate.substring(5,7);
+        let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+        return months[monthNumber - 1];
+	  }
+
+	  function addDaysOfWeek() {
+		  $("td[headers='Monday'] .inner .day").each(function() {
+			  if (!isNaN($(this).text().trim())) {
+				  let fullDate = $(this).closest("td").attr("date-date"); 
+				  let shortMonthName = getMonthName(fullDate);
+				  let oldtext = $(this).text().trim();
+				  let newtext = "Mon ".concat(oldtext, " ", shortMonthName);
+				  $(this).text(newtext);
+			  }
+		  });  
+		  $("td[headers='Tuesday'] .inner .day").each(function() {
+			  if (!isNaN($(this).text().trim())) {
+				  let fullDate = $(this).closest("td").attr("date-date"); 
+				  let shortMonthName = getMonthName(fullDate);
+				  let oldtext = $(this).text().trim();
+				  let newtext = "Tue ".concat(oldtext, " ", shortMonthName);
+				  $(this).text(newtext);
+			  }
+		  });
+		  $("td[headers='Wednesday'] .inner .day").each(function() {
+			  if (!isNaN($(this).text().trim())) {
+				  let fullDate = $(this).closest("td").attr("date-date"); 
+				  let shortMonthName = getMonthName(fullDate);
+				  let oldtext = $(this).text().trim();
+				  let newtext = "Wed ".concat(oldtext, " ", shortMonthName);
+				  $(this).text(newtext);
+			  }
+		  });
+		  $("td[headers='Thursday'] .inner .day").each(function() {
+			  if (!isNaN($(this).text().trim())) {
+				  let fullDate = $(this).closest("td").attr("date-date"); 
+				  let shortMonthName = getMonthName(fullDate);
+				  let oldtext = $(this).text().trim();
+				  let newtext = "Thu ".concat(oldtext, " ", shortMonthName);
+				  $(this).text(newtext);
+			  }
+		  });
+		  $("td[headers='Friday'] .inner .day").each(function() {
+			  if (!isNaN($(this).text().trim())) {
+				  let fullDate = $(this).closest("td").attr("date-date"); 
+				  let shortMonthName = getMonthName(fullDate);
+				  let oldtext = $(this).text().trim();
+				  let newtext = "Fri ".concat(oldtext, " ", shortMonthName);
+				  $(this).text(newtext);
+			  }
+		  });
+		  $("td[headers='Saturday'] .inner .day").each(function() {
+			  if (!isNaN($(this).text().trim())) {
+				  let fullDate = $(this).closest("td").attr("date-date"); 
+				  let shortMonthName = getMonthName(fullDate);
+				  let oldtext = $(this).text().trim();
+				  let newtext = "Sat ".concat(oldtext, " ", shortMonthName);
+				  $(this).text(newtext);
+			  }
+		  });
+		  $("td[headers='Sunday'] .inner .day").each(function() {
+			  if (!isNaN($(this).text().trim())) {
+				  let fullDate = $(this).closest("td").attr("date-date"); 
+				  let shortMonthName = getMonthName(fullDate);
+				  let oldtext = $(this).text().trim();
+				  let newtext = "Sun ".concat(oldtext, " ", shortMonthName);
+				  $(this).text(newtext);
+			  }
+		  });
+		  
+	  }
+	  
+	  changeofmonth();
+	  addDaysOfWeek();
+	
+	  
+      $('#block-lalgcalendarlegend li', context).each(function () {
+		// this is for each of the coloured group types at the top...
+		$(this).on('click', function() {
+			$('#block-lalgcalendarlegend li').removeClass("group_type_show");
+			$(this).addClass("group_type_show");
+	
+			// when a particular coloured group type at the top is clicked...
+			const typeClicked = $(this).text().trim().toLowerCase().split(" ").join("");
+			// eg. typeClicked = "sports&games"
+			const mainCalendarItems = $('.month-view').find('.item');
+			// every single event on the calendar...
+			if (typeClicked == "allgrouptypes") {
+				mainCalendarItems.each(function() {		
+					$(this).show();
+				});
+			} else {
+				mainCalendarItems.each(function() {				
+					const divWithPageType = $(this).find('.monthview');
+					// divWithPageType is the group type class of the event
+					if (!divWithPageType.hasClass(typeClicked)) {
+						$(this).hide();
+					} else {
+						$(this).show();
+					}	
+				});
+			}
+
+		});
+      });
+    }
+  };
 
   Drupal.behaviors.lalg_calendar = {
     attach: function (context, settings) {
-      // When displaying the calendar, add a class which represents the group type
-      $(".hide").each(function() {
-        $(this).parents(".calendar").addClass($(this).text().trim().toLowerCase().split(" ").join(""));
-      });  
+
 	  
 	  $(".hidefc").each(function() {
         $(this).parents(".fc-content").addClass($(this).text().trim().toLowerCase().split(" ").join(""));
