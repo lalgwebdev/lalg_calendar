b2e26de99af30c0a1bcb590d7b47bce4a2504f17
lalgwebdev <47321570+lalgwebdev@users.noreply.github.com>
2024-10-08 08:54:29 +0100

    js for months

---
+++ js/lalg-previous-months-dropdown.js
@@ -0,0 +1,31 @@
+var select = document.getElementById("lalg-calendar-specific-month");
+
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+const date = new Date();
+let current_month_number = date.getMonth();
+let current_month = ((current_month_number + 1).toString()).padStart(2, '0');
+let current_year = date.getFullYear().toString();
+let current_year_month = current_year.concat(current_month);
+date.setMonth(date.getMonth() -7);
+
+months_array = [];
+months_array["0"] = "Jump to Month...";
+
+for (var i = 0; i < 13; i++) {
+  date.setMonth(date.getMonth() +1);
+  let month_number = date.getMonth();
+  let month = ((month_number + 1).toString()).padStart(2, '0');
+  let year = date.getFullYear().toString();
+  let year_month = year.concat(month);
+  if (current_year_month == year_month) {
+     current = " (current)";
+  } else {
+     current = "";
+  }
+  months_array[year_month] = months[month_number].concat(" ").concat(year).concat(current);
+}
+
+for(i in months_array) {
+    select.options[select.options.length] = new Option(months_array[i],i);
+}
+
